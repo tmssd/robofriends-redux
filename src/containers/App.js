5b0a7a2c64feaecb60076d21b893486eaf16b96f
tmssd <67862896+tmssd@users.noreply.github.com>
2022-10-21 18:07:27 +0300

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,40 +1,73 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+import { requestRobots, setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+    // console.log(state);
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error,
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    // console.log(dispatch);
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        // onRequestRobots: () => requestRobots(dispatch), // it's the same as () => dispatch(requestRobots());
+        onRequestRobots: () => dispatch(requestRobots()),
+    };
+}
+
 class App extends Component {
-    constructor() {
+// not needed anymore, because all the 'state' is in reducers.js file now
+/*     constructor() {
         super();
         this.state = {
             robots: [],
             searchfield: '',
         }
     }
-
+ */
     componentDidMount() {
+        this.props.onRequestRobots();
+    }
+
+// not needed anymore
+/*     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => this.setState({ robots: users }));
     }
+ */
 
-    onSearchChange = (event) => {
+// not needed anymore
+/*     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
     }
+ */
 
     render() {
-        const { robots, searchfield } = this.state;
+        // const { robots, searchfield } = this.state; // not needed anymore
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
-        return !robots.length ?
+        // return !robots.length ? // not needed anymore
+        return isPending ?
             <h1 className="tc">Loading</h1> :
             (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
                             <CardList robots={filteredRobots} />
@@ -45,4 +78,5 @@ class App extends Component {
     };
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+// console.log(connect)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
+const logger = createLogger();
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
